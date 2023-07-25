2b844e2710ff06e964ef9fc82e464fa9a8dd51db
Brecht <brechtdv@gmail.com>
2023-07-25 22:58:00 +0200

    remove package name

--- R/plots.R
+++ R/plots.R
@@ -9,7 +9,7 @@ function(x, title) {
   if (!("DATA" %in% names(x))) stop("Input 'x' requires 'DATA' variable.")
 
   # merge data
-  FERG2:::map1$DATA <- x$DATA[match(FERG2:::map1$ISO_3_CODE, x$ISO3)]
+  map1$DATA <- x$DATA[match(map1$ISO_3_CODE, x$ISO3)]
 
   # settings
   col_na <- rgb(210, 210, 210, max = 255)
@@ -18,21 +18,21 @@ function(x, title) {
   col_lk <- rgb(190, 210, 255, max = 255)
 
   # define colors
-  breaks <- pretty(FERG2:::map1$DATA)
-  cat <- cut(FERG2:::map1$DATA, breaks)
+  breaks <- pretty(map1$DATA)
+  cat <- cut(map1$DATA, breaks)
   col <- RColorBrewer::brewer.pal(length(levels(cat)), "Reds")
   cols <- col[cat]
   cols[is.na(cols)] <- col_na
 
   # setup plot
   par(mar = c(0, 0, 0, 0))
-  plot(st_geometry(FERG2:::map1), col = cols, bg = col_bg, border = col_br, lwd = .75)
-  plot(st_geometry(FERG2:::map3), add = T, lwd = .75,
+  plot(st_geometry(map1), col = cols, bg = col_bg, border = col_br, lwd = .75)
+  plot(st_geometry(map3), add = T, lwd = .75,
      col = c(col_na, col_bg, col_na),
      border = c(col_br, col_lk, col_br))
-  plot(st_geometry(FERG2:::map2), add = T, lwd = .75,
+  plot(st_geometry(map2), add = T, lwd = .75,
        col = "white", lty = 1)
-  plot(st_geometry(FERG2:::map2), add = T, lwd = .75,
+  plot(st_geometry(map2), add = T, lwd = .75,
        col = rgb(110, 110, 110, max = 255),
        lty = c(3, 3, 1, 3, 3, 3, 3, 3, 3, 3))
 
