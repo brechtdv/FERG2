6aa405f7852b84759f2a647aa523dac2ebda0c24
Brecht <brechtdv@gmail.com>
2023-07-27 13:16:06 +0200

    allow plotting factor data

--- R/plots.R
+++ R/plots.R
@@ -3,13 +3,16 @@
 ## world map
 
 plot_world <-
-function(x, title) {
+function(x, iso3 = "ISO3", data = "DATA", col.pal = "Reds", cols = NULL,
+  legend.labs = NULL, legend.title = NULL, legend.ncol = 3) {
   # check arguments
-  if (!("ISO3" %in% names(x))) stop("Input 'x' requires 'ISO3' variable.")
-  if (!("DATA" %in% names(x))) stop("Input 'x' requires 'DATA' variable.")
+  if (!(iso3 %in% names(x)))
+    stop(sprintf("Input 'x' requires '%s' variable.", iso3))
+  if (!(data %in% names(x)))
+    stop(sprintf("Input 'x' requires '%s' variable.", data))
 
   # merge data
-  map1$DATA <- x$DATA[match(map1$ISO_3_CODE, x$ISO3)]
+  map1$DATA <- x[[data]][match(map1$ISO_3_CODE, x[[iso3]])]
 
   # settings
   col_na <- rgb(210, 210, 210, max = 255)
@@ -18,10 +21,30 @@ function(x, title) {
   col_lk <- rgb(190, 210, 255, max = 255)
 
   # define colors
-  breaks <- pretty(map1$DATA)
-  cat <- cut(map1$DATA, breaks)
-  col <- RColorBrewer::brewer.pal(length(levels(cat)), "Reds")
-  cols <- col[cat]
+  if (is.numeric(x[[data]])) {
+    breaks <- pretty(map1$DATA)
+    cat <- cut(map1$DATA, breaks)
+    if (length(col.pal) == 1) {
+      col <- RColorBrewer::brewer.pal(length(levels(cat)), col.pal)
+    } else {
+      col <- col.pal
+    }
+    cols <- col[cat]
+    
+    if (is.null(legend.labs))
+      legend.labs <- levels(cat)
+
+  } else if (is.factor(x[[data]])) {
+    if (length(col.pal) == 1) {
+      col <- RColorBrewer::brewer.pal(length(levels(cat)), col.pal)
+    } else {
+      col <- col.pal
+    }
+    cols <- col[map1$DATA]
+
+    if (is.null(legend.labs))
+      legend.labs <- levels(map1$DATA)
+  }
   cols[is.na(cols)] <- col_na
 
   # setup plot
@@ -38,10 +61,10 @@ function(x, title) {
 
   # add legend
   legend("bottomleft",
-    title = title,
+    title = legend.title,
     box.col = NA, bg = NA, 
-    legend = c(levels(cat), "Not applicable"),
+    legend = c(legend.labs, "Not applicable"),
     fill = c(col, col_na),
     cex = .9, y.intersp = .9, x.intersp = 0.5, text.width = 20,
-    ncol = 3)
+    ncol = legend.ncol)
 }
