63f572ef0c0ddaa0d6a9192979b75d8ff8a4f51d
Louise Vaes <louise.vaes@sciensano.be>
2024-12-03 14:38:36 +0100

    Update package with LE table

--- data-raw/data.R
+++ data-raw/data.R
@@ -9,6 +9,7 @@ library(sf)
 library(tidyr)
 library(dplyr)
 library(data.table)
+library(readxl)
 
 ## country definitions
 countries <- readxl("WHO_countries_subregions.xlsx")
@@ -16,6 +17,7 @@ countries <- readxl("WHO_countries_subregions.xlsx")
 ## UN WPP population data
 ## 1950-2021 only available on Brecht laptop
 ## 1950-2023 downloaded from https://population.un.org/wpp/Download/Standard/CSV/ on 22/07/24
+## 1950-2023 downloaded from https://population.un.org/wpp/Download/Standard/Mortality/ on 03/12/24 to add LE
 if (FALSE) {
 # pop <-
   # read.csv("WPP2022_Population1JanuaryBySingleAgeSex_Medium_1950-2021.csv")
@@ -49,6 +51,36 @@ pop <-
   read.csv("WPP2022_Population1JanuaryBySingleAgeSex_Medium_1990-2023.csv")
 str(pop)
 
+LE <-
+  read_excel("WPP2024_MORT_F05_1_LIFE_EXPECTANCY_BY_AGE_BOTH_SEXES.xlsx", skip=16, col_types = "text")
+LE <- LE %>%
+  rename(ISO3 = `ISO3 Alpha-code`)
+LE <- subset(LE, ISO3 %in% countries$ISO3)
+LE <- subset(LE, Year >= 1990)
+LE <- subset(LE,
+              select = -c(Index, Variant, `Region, subregion, country or area *`, Notes, `Location code`,
+                          `ISO2 Alpha-code`, `SDMX code**`, `Type`, `Parent code`))
+colnames(LE) <- paste("A", colnames(LE), sep = "_")
+LE <- LE %>%
+  rename(ISO3 = A_ISO3, 
+         YEAR = A_Year)
+LE <- 
+  pivot_longer(
+    LE,
+    cols = starts_with("A_"),
+    names_to = "AGE",
+    values_to = "LE")
+
+LE$AGE <- sub(".*_", "", LE$AGE)
+
+life_exp <- LE
+
+write.csv(life_exp,
+          row.names = FALSE,
+          file = "WHO_life_expectancY_AGE_1990_2023.csv")
+
+
+
 ## WHO shapefiles
 map1 <- st_read("shp/general_2013.shp")
 map2 <- st_read("shp/maskline_general_2013.shp")
@@ -60,6 +92,6 @@ map3 <- st_read("shp/maskpoly_general_2013.shp")
 
 save(
   countries,
-  pop,
+  pop, life_exp,
   map1, map2, map3,
   file = "../R/sysdata.rda")
