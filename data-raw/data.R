84b87b716231b6e1377c0af0fbf2f34a0ef843a6
LouiseVaes <118883304+LouiseVaes@users.noreply.github.com>
2025-02-25 16:29:24 +0100

    Add population data IHME

--- data-raw/data.R
+++ data-raw/data.R
@@ -13,6 +13,7 @@ library(tidyr)
 library(dplyr)
 library(data.table)
 library(readxl)
+library(stringr)
 
 ## country definitions
 countries <- readxl("WHO_countries_subregions.xlsx")
@@ -157,6 +158,143 @@ write.csv(pop_1year,
           row.names = FALSE,
           file = "IHME_pop_1y_1990_2021.csv")
 
+## IHME population data all
+IHME_pop_1 <-
+  read.csv("IHME-GBD_2021_DATA-95f2786f-1.csv")
+IHME_pop_2 <-
+  read.csv("IHME-GBD_2021_DATA-95f2786f-2.csv")
+
+IHME_pop <- rbind(IHME_pop_1, IHME_pop_2)
+IHME_pop <- IHME_pop[,c("location_name", "sex_name", "age_name","year", "val")]
+
+IHME_pop <- IHME_pop %>%
+  mutate(COUNTRY = case_when(
+    location_name == "Türkiye" ~ "Turkiye",
+    location_name == "Turkey" ~ "Turkiye",
+    location_name == "Congo" ~ "Congo (the)",
+    location_name == "Democratic Republic of the Congo" ~ "Congo (the Democratic Republic of the)",
+    location_name == "Dominican Republic" ~ "Dominican Republic (the)",
+    location_name == "Gambia" ~ "Gambia (the)",
+    location_name == "Lao People's Democratic Republic" ~ "Lao People's Dem. Republic",
+    location_name == "Micronesia (Federated States of)" ~ "Micronesia (Fed. States of)",
+    location_name == "Democratic People's Republic of Korea" ~ "Korea (the Democratic People's Republic of)",
+    location_name == "Republic of Korea" ~ "Korea (the republic of)",
+    location_name == "Arab Republic of Egypt" ~ "Egypt",
+    location_name == "Argentine Republic" ~ "Argentina",
+    location_name == "Bolivarian Republic of Venezuela" ~ "Venezuela (Bolivarian Republic of)",
+    location_name == "Czech Republic" ~ "Czechia",
+    location_name == "Democratic Republic of Sao Tome and Principe" ~ "Sao Tome and Principe",
+    location_name == "Democratic Republic of Timor-Leste" ~ "Timor-Leste",
+    location_name == "Democratic Socialist Republic of Sri Lanka" ~ "Sri Lanka",
+    location_name == "Eastern Republic of Uruguay" ~ "Uruguay",
+    location_name == "Federal Democratic Republic of Ethiopia" ~ "Ethiopia",
+    location_name == "Federal Democratic Republic of Nepal" ~ "Nepal",
+    location_name == "Federal Republic of Germany" ~ "Germany",
+    location_name == "Federal Republic of Nigeria" ~ "Nigeria",
+    location_name == "Federal Republic of Somalia" ~ "Somalia",
+    location_name == "Federated States of Micronesia" ~ "Micronesia (Fed. States of)",
+    location_name == "Federative Republic of Brazil" ~ "Brazil",
+    location_name == "French Republic" ~ "France",
+    location_name == "Gabonese Republic" ~ "Gabon",
+    location_name == "Grand Duchy of Luxembourg" ~ "Luxembourg",
+    location_name == "Hashemite Kingdom of Jordan" ~ "Jordan",
+    location_name == "Independent State of Papua New Guinea" ~ "Papua New Guinea",
+    location_name == "Independent State of Samoa" ~ "Samoa",
+    location_name == "Islamic Republic of Afghanistan" ~ "Afghanistan",
+    location_name == "Islamic Republic of Iran" ~ "Iran (Islamic Republic of)",
+    location_name == "Islamic Republic of Mauritania" ~ "Mauritania",
+    location_name == "Islamic Republic of Pakistan" ~ "Pakistan",
+    location_name == "Kingdom of Bahrain" ~ "Bahrain",
+    location_name == "Kingdom of Belgium" ~ "Belgium",
+    location_name == "Kingdom of Bhutan" ~ "Bhutan",
+    location_name == "Kingdom of Cambodia" ~ "Cambodia",
+    location_name == "Kingdom of Denmark" ~ "Denmark",
+    location_name == "Kingdom of Eswatini" ~ "Eswatini",
+    location_name == "Kingdom of Lesotho" ~ "Lesotho",
+    location_name == "Kingdom of Morocco" ~ "Morocco",
+    location_name == "Kingdom of Norway" ~ "Norway",
+    location_name == "Kingdom of Saudi Arabia" ~ "Saudi Arabia",
+    location_name == "Kingdom of Spain" ~ "Spain",
+    location_name == "Kingdom of Sweden" ~ "Sweden",
+    location_name == "Kingdom of Thailand" ~ "Thailand",
+    location_name == "Kingdom of the Netherlands" ~ "Netherlands",
+    location_name == "Kingdom of Tonga" ~ "Tonga",
+    location_name == "Kyrgyz Republic" ~ "Kyrgyzstan",
+    location_name == "Lebanese Republic" ~ "Lebanon",
+    location_name == "People's Democratic Republic of Algeria" ~ "Algeria",
+    location_name == "People's Republic of Bangladesh" ~ "Bangladesh",
+    location_name == "People's Republic of China" ~ "China",
+    location_name == "Plurinational State of Bolivia" ~ "Bolivia (Plurinational State of)",
+    location_name == "Portuguese Republic" ~ "Portugal",
+    location_name == "Republic of the Congo" ~ "Congo (the)",
+    location_name == "Republic of the Gambia" ~ "Gambia (the)",
+    location_name == "Republic of the Marshall Islands" ~ "Marshall Islands",
+    location_name == "Republic of the Niger" ~ "Niger",
+    location_name == "Republic of the Philippines" ~ "Philippines",
+    location_name == "Republic of the Union of Myanmar" ~ "Myanmar",
+    location_name == "Republic of Turkey" ~ "Turkiye",
+    location_name == "Republic of Moldova" ~ "Republic of Moldova",
+    location_name == "Commonwealth of Dominica" ~ "Dominica",
+    substr(location_name, 1, 12) == "Republic of " ~ substr(location_name,13, str_count(location_name)),
+    location_name == "Principality of Andorra" ~ "Andorra",
+    location_name == "Principality of Monaco" ~ "Monaco",
+    location_name == "Slovak Republic" ~ "Slovakia",
+    location_name == "Socialist Republic of Viet Nam" ~ "Viet Nam",
+    substr(location_name, 1, 9) == "State of " ~ substr(location_name, 10, str_count(location_name)),
+    location_name == "Sultanate of Oman" ~ "Oman",
+    location_name == "Swiss Confederation" ~ "Switzerland",
+    location_name == "Togolese Republic" ~ "Togo",
+    location_name == "Union of the Comoros" ~ "Comoros",
+    location_name == "United Kingdom of Great Britain and Northern Ireland" ~ "United Kingdom",
+    location_name == "United Mexican States" ~ "Mexico",
+    location_name == "Commonwealth of the Bahamas" ~ "Bahamas",
+    location_name == "Hellenic Republic" ~ "Greece",
+    .default = location_name
+  ))
+
+
+IHME_pop <- subset(IHME_pop, COUNTRY %in% countries$COUNTRY)
+IHME_pop <- merge(IHME_pop, countries[,c("COUNTRY","ISO3")])
+IHME_pop <- IHME_pop[,c("ISO3","year","age_name","sex_name","val")]
+colnames(IHME_pop) <- c("ISO3", "YEAR", "age_name", "SEX", "POP")
+IHME_pop <- subset(IHME_pop, age_name %in% c("<1 year", "2-4 years", "5-9 years",
+                                        "10-14 years", "15-19 years", "20-24 years", "25-29 years",
+                                        "30-34 years", "35-39 years", "40-44 years", "45-49 years",
+                                        "50-54 years", "55-59 years", "60-64 years", "65-69 years",
+                                        "70-74 years", "75-79 years", "80-84 years", "85-89 years",
+                                        "90-94 years", "95+ years"))
+IHME_pop <- IHME_pop %>% 
+  mutate(AGE = case_when(
+    age_name == "<1 year" ~ "0-1", 
+    age_name == "2-4 years"~ "2-4", 
+    age_name == "5-9 years" ~ "5-9",
+    age_name == "10-14 years" ~ "10-14",
+    age_name == "15-19 years" ~ "15-19",
+    age_name == "20-24 years" ~ "20-24",
+    age_name == "25-29 years" ~ "25-29",
+    age_name == "30-34 years" ~ "30-34",
+    age_name == "35-39 years" ~ "35-39",
+    age_name == "40-44 years" ~ "40-44",
+    age_name == "45-49 years" ~ "45-49",
+    age_name == "50-54 years" ~ "50-54",
+    age_name == "55-59 years" ~ "55-59",
+    age_name == "60-64 years" ~ "60-64",
+    age_name == "65-69 years" ~ "65-69",
+    age_name == "70-74 years" ~ "70-74",
+    age_name == "75-79 years" ~ "75-79",
+    age_name == "80-84 years" ~ "80-84",
+    age_name == "85-89 years" ~ "85-125",
+    age_name == "90-94 years" ~ "85-125",
+    age_name == "95+ years" ~ "85-125"))
+
+IHME_pop$age_naem <- NULL
+IHME_pop <- IHME_pop %>% 
+  group_by(ISO3, YEAR, SEX, AGE) %>% 
+  summarise(POP = sum(POP))
+
+IHME_pop <- arrange(IHME_pop, ISO3, YEAR, SEX, readr::parse_number(AGE))
+pop_IHME <- IHME_pop
+
 ## WHO shapefiles
 map1 <- st_read("shp/general_2013.shp")
 map2 <- st_read("shp/maskline_general_2013.shp")
@@ -171,9 +309,11 @@ map3 <- st_read("shp/maskpoly_general_2013.shp")
 # life_exp <- FERG2:::life_exp
 # life_birth <- FERG2:::life_birth
 # pop_1year <- FERG2:::pop_1year
+# pop_IHME <- FERG2:::pop_IHME
 
 save(
-  countries, pop, pop_1year,
+  countries, 
+  pop, pop_1year, pop_IHME,
   life_exp,life_birth,
   map1, map2, map3,
   file = "../R/sysdata.rda")
